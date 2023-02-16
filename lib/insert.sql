fe61beafa3e39c9f5b0c44542426505c611ef65b
Skanner33 <mathewmageto@gmail.com>
2023-02-16 18:24:30 +0300

    completed queries

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Mr.Choclate', 20, 'M', 'Brown', 'Calm', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Rowdy', 24, 'M', 'Black', 'Rowdy', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Tabitha', 29, 'F', 'Burgundy', 'Carefree', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Sergent Brown', 50, 'M', 'Black', 'Neutral', 'No');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Melissa', 30, 'F', 'Grey', 'Sociable', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Grinch', 60, 'M', 'Green', 'Persistent', 'No');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES('Wendy', 18, 'F', 'Light Brown', 'Active', 'Yes');
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES(null, 20, 'M', 'black', 'angry', 0);
